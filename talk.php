3997586e06f1dfdf77aef388a75d3ffbeb381331
Don <evidence@384.jp>
2023-06-21 15:13:06 +0900

    偉業

--- talk.php
+++ talk.php
@@ -110,5 +110,9 @@ function airrep($content) {
 		$mesary = array('ええで', 'ええんやで', 'あかんに決まっとるやろ');
 		$res = $mesary[rand(0, count($mesary) - 1)];
 	}
+	else if (preg_match('/(ほめて|褒めて)$/', $content)) {
+		$mesary = array('えらいやで', '偉業やで', 'すごいやん');
+		$res = $mesary[rand(0, count($mesary) - 1)];
+	}
 	return $res;
 }
