3a41e7420ce4df3add875f2a66781c59378bc418
Don <evidence@384.jp>
2023-06-26 07:04:41 +0900

    ありがとう

--- talk.php
+++ talk.php
@@ -16,34 +16,6 @@ function talk($content) {
 		$res .= 'ラッキーゴーストは『'. $rss->channel->item[$index]->title. '』やで'. "\n";
 		$res .= $rss->channel->item[$index]->link;
 	}
-	else if (preg_match('/おすすめ|オススメ|お勧め|お薦め/', $content)) {
-		$url = 'http://buynowforsale.shillest.net/ghosts/ghosts/index.rss';
-		$rss = simplexml_load_file($url);
-		$index = mt_rand(0, $rss->channel->item->count() - 1);
-		$res = $rss->channel->item[$index]->title. "\n".$rss->channel->item[$index]->link;
-	}
-	else if (preg_match('/doc/i', $content)) {
-		$res = 'http://ssp.shillest.net/ukadoc/manual/';
-	}
-	else if (preg_match('/yaya/i', $content)) {
-		$res = 'https://emily.shillest.net/ayaya/';
-	}
-	else if (preg_match('/里々|satori/i', $content)) {
-		$res = 'https://soliton.sub.jp/satori/';
-	}
-	else if (preg_match('/ソース|コード|GitHub|リポジトリ|中身/i', $content)) {
-		$res = 'https://github.com/nikolat/nostr-webhook-php-sample';
-	}
-	else if (preg_match('/時刻|時報/', $content)) {
-		$date = new DateTime('now');
-		$week = array('日', '月', '火', '水', '木', '金', '土');
-		$weekday = $date->format('w');
-		$res = $date->format('Y年m月d日 H時i分s秒 ').$week[$weekday].'曜日やで';
-	}
-	else if (preg_match('/ログボ|ログインボーナス/', $content)) {
-		$mesary = array('ログボとかあらへん', '継続は力やな', '今日もログインしてえらいやで');
-		$res = $mesary[rand(0, count($mesary) - 1)];
-	}
 	else if (preg_match('/いいの?か?(？|\?)$/u', $content)) {
 		$mesary = array('ええで', 'ええんやで', 'あかんに決まっとるやろ');
 		$res = $mesary[rand(0, count($mesary) - 1)];
@@ -94,8 +66,34 @@ function talk($content) {
 		$res = str_replace('\n', "\n", $res);
 		$res .= "\n\n（※出典：気象庁ホームページ）";
 	}
+	else if (preg_match('/doc/i', $content)) {
+		$res = 'http://ssp.shillest.net/ukadoc/manual/';
+	}
+	else if (preg_match('/yaya/i', $content)) {
+		$res = 'https://emily.shillest.net/ayaya/';
+	}
+	else if (preg_match('/里々|satori/i', $content)) {
+		$res = 'https://soliton.sub.jp/satori/';
+	}
+	else if (preg_match('/ソース|コード|GitHub|リポジトリ|中身/i', $content)) {
+		$res = 'https://github.com/nikolat/nostr-webhook-php-sample';
+	}
+	else if (preg_match('/時刻|時報/', $content)) {
+		$date = new DateTime('now');
+		$week = array('日', '月', '火', '水', '木', '金', '土');
+		$weekday = $date->format('w');
+		$res = $date->format('Y年m月d日 H時i分s秒 ').$week[$weekday].'曜日やで';
+	}
+	else if (preg_match('/ログボ|ログインボーナス/', $content)) {
+		$mesary = array('ログボとかあらへん', '継続は力やな', '今日もログインしてえらいやで');
+		$res = $mesary[rand(0, count($mesary) - 1)];
+	}
 	else if (preg_match('/(？|\?)$/', $content)) {
 		$mesary = array('ワイに聞かれても', '知らんて', 'せやな');
+		$res = $mesary[rand(0, count($mesary) - 1)];
+	}
+	else if (preg_match('/ありがと|(たす|助)か(る|った)/', $content)) {
+		$mesary = array('ええってことよ', '礼はいらんで', 'かまへん');
 		$res = $mesary[rand(0, count($mesary) - 1)];
 	}
 	else if (preg_match('/えんいー/', $content)) {
