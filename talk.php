686db7a93bd1b27aac6d84df2d6ac7e5a645d8f1
Don <evidence@384.jp>
2023-07-14 11:39:40 +0900

    kwsk

--- talk.php
+++ talk.php
@@ -41,7 +41,7 @@ function talk($content) {
 		}
 	}
 	else if (preg_match('/いいの?か?(？|\?)$/u', $content)) {
-		if (preg_match('/何|なに|誰|だれ|どこ|いつ/u', $content)) {
+		if (preg_match('/何|なに|誰|だれ|どこ|いつ|どう|どの/u', $content)) {
 			$mesary = array('難しいところやな', '自分の信じた道を進むんや', '知らんがな');
 			$res = $mesary[rand(0, count($mesary) - 1)];
 		}
@@ -160,7 +160,7 @@ function talk($content) {
 function airrep($content, $emojiTags) {
 	$res = 'えんいー';
 	if (preg_match('/いいの?か?(？|\?)$/u', $content)) {
-		if (preg_match('/何|なに|誰|だれ|どこ|いつ/u', $content)) {
+		if (preg_match('/何|なに|誰|だれ|どこ|いつ|どう|どの/u', $content)) {
 			$mesary = array('難しいところやな', '自分の信じた道を進むんや', '知らんがな');
 			$res = $mesary[rand(0, count($mesary) - 1)];
 		}
